2180c63e8c2bd99db8c6661570c69be4f379db03
traducer <traducer21@gmail.com>
2017-03-27 11:14:25 -0700

    cleaning up item view a bit, needs to be refactored and styled

--- src/views/itemView.js
+++ src/views/itemView.js
@@ -1,29 +1,36 @@
 const { h } = require('hyperapp')
 const classnames = require('classnames')
+const Container = require('../components/container')
 
 const itemView = module.exports = (model, actions) => {
   const id = model.router.params.id
   const item = model.items[id]
   const { loading } = model
 
+  const onCreate = e => {
+    actions.fetchItemAndComments(id)
+  }
+
   const Comment = ({ item }) => (
     <div>
-        <p>{item.type === 'comment' && item.text}</p>
-        <br />
-        {item.kids && item.kids.map(id => <Comment item={model.items[id]} />)}
+      <p>{item.type === 'comment' && item.text}</p>
+      <br />
+      {item.kids && item.kids.map(id => model.items[id] && <Comment item={model.items[id]} />)}
     </div>
   )
 
   const Header = ({ item, loading }) => (
     <div>
-      {!loading && item && <Comment item={item} />}
+      <Comment item={item} />
     </div>
   )
 
   return (
-    <div onCreate={() => actions.fetchItemAndComments(id) }>
-      {!loading && item && <Header item={item} loading={loading} />}
-      {loading && <div class={classnames({ loading: loading })} />}
-    </div>
+    <Container actions={actions} type={null} loading={model.loading}>
+      <div class='item' onCreate={onCreate}>
+        {!loading && item && <Header item={item} loading={loading} />}
+        {loading && <div class={classnames({ loading })} />}
+      </div>
+    </Container>
   )
 }
