617c7955b23ec10aa3e0cf2150baef77bbcbd70b
image72 <image72@msn.com>
2018-07-18 15:23:28 +0800

    optimize bundler size

--- src/database.js
+++ src/database.js
@@ -1,4 +1,6 @@
-import * as firebase from 'firebase';
+import * as firebase from 'firebase/app';
+import 'firebase/storage';
+import 'firebase/database';
 
 const config = {
   databaseURL: 'https://hacker-news.firebaseio.com'
